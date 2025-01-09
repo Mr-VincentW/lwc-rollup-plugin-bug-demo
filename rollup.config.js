aba08577173ec346a7451cbba90e664b45bba6d0
Vincent W <vincentwang863@gmail.com>
2025-01-10 11:53:14 +1300

    Added a missing npm package.

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import html from '@rollup/plugin-html';
 import livereload from 'rollup-plugin-livereload';
 import lwc from '@lwc/rollup-plugin';
 import serve from 'rollup-plugin-serve';
+import replace from '@rollup/plugin-replace';
 
 export default args => ({
   input: 'src/main.js',
@@ -10,6 +11,12 @@ export default args => ({
     format: 'esm'
   },
   plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(
+        process.env.NODE_ENV ?? 'development'
+      ),
+      preventAssignment: true
+    }),
     html(),
     lwc(),
     ...(args.watch
